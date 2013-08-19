c52c956c0aa97f57af4a35a7d1874281bdffd0f6
Matthew Iannucci <mpiannucci@gmail.com>
2013-08-19 12:39:24 -0400

    bug fixes

--- code.py
+++ code.py
@@ -209,7 +209,7 @@ class Like:
         post = model.get_post(int(id))
         like = post.likes
         model.like_post(int(id), int(like))
-        raise web.seeother('/blog')
+        raise web.seeother('/blog/')
 
 class Dislike:
     """ Create the method to dislike a post """
@@ -217,7 +217,7 @@ class Dislike:
         post = model.get_post(int(id))
         dlike = post.dislikes
         model.dislike_post(int(id), int(dlike))
-        raise web.seeother('/blog')
+        raise web.seeother('/blog/1')
 
 def notfound():
     """ Create the not found page """
